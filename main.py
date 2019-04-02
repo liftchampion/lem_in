103e4b9db716fe19918bf188a536a7bc0142912e
ggerardy <liftchampion@gmail.com>
2019-04-02 09:28:43 +0300

    Fix .gitignore - add venv Fix main.py - nodes' names translated into numbers (very slow, not for release)

--- main.py
+++ main.py
@@ -30,13 +30,80 @@ for i in range(len(s)):
     if rooms:
         list_room.append(s[i][:s[i].find(' ')])
     elif '-' in s[i]:
-        #       print 'append ' + s[i][s[i].find('-') + 1:] + ' in ' + s[i][:s[i].find('-')]
+        # print 'append ' + s[i][s[i].find('-') + 1:] + ' in ' + s[i][:s[i].find('-')]
         room_tree[s[i][:s[i].find('-')]].append(s[i][s[i].find('-') + 1:] + '_____')
         room_tree[s[i][s[i].find('-') + 1:]].append(s[i][:s[i].find('-')] + '_____')
-        #       print room_tree
-        #       print ''
+        # print room_tree
+        # print ''
 
-#print room_tree
+# print room_tree
 print "ant count: " + str(kolichestvo_muraviev)
 print "start is: " + start + " routes - " + str(len(room_tree[start]) - 1)
 print "end is : " + end + " routes - " + str(len(room_tree[end]) - 1)
+
+
+def map_to_arr(graph):
+    res = []
+    j = 0
+    for key in graph:
+        res.append(key)
+        j += 1
+    return res
+
+
+idx_to_name = map_to_arr(room_tree)
+
+
+def get_idx(val):
+    j = 0
+    for n in idx_to_name:
+        if val == n:
+            return j
+        j += 1
+
+
+def fill_links(graph):
+    res = []
+    j = 0
+    for key in graph:
+        res.append([])
+        for value in graph[key]:
+            res[j].append(get_idx(value))
+        j += 1
+    return res
+
+
+fill_links(room_tree)
+grph = fill_links(room_tree)
+ns = get_idx(start)
+ne = get_idx(end)
+print "NEW start is: " + str(ns)
+print "NEW end is: " + str(ne)
+# print grph
+
+
+def bell_ford(graph):
+    n = 3
+    m = 4
+    a = [[0] * m for j in range(n)]
+
+
+bell_ford(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
