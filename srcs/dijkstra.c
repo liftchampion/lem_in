92fd5d5d5db1c841cb1108fde3782ec527fc1925
Benfrey frey Miklaz <bmiklaz@vo-h2.21-school.ru>
2019-04-08 21:40:04 +0300

    dijkstra

---
+++ srcs/dijkstra.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dijkstra.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmiklaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/08 18:39:09 by bmiklaz           #+#    #+#             */
+/*   Updated: 2019/04/08 18:39:16 by bmiklaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in_structs.h"
+
+void	sift_down(t_heap *heap, int ind)
+{
+
+}
+
+void	sift_up(t_heap *heap, int ind)
+{
+
+}
+
+t_edge	*take_min(t_heap *heap)
+{
+
+}
+
+t_heap	*make_heap()
+{
+
+}
+
+void	dijkstra()
+{
+
+}
