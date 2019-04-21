39dae029929ba788a0695cf4797f1aa88469ac8a
ggerardy <liftchampion@gmail.com>
2019-04-21 23:20:31 +0300

    Reverting from unssucceded bug-finding - conflicts in main.py solved to prev
    Took 7 minutes

--- main.py
+++ main.py
@@ -16,26 +16,65 @@ import heapq
 
 s = sys.stdin.read().split('\n')
 
-start_end = s[0].split(' ')
-start = int(start_end[0])
-end = int(start_end[1])
+kolichestvo_muraviev = int(s[0])
 s = s[1:]
 
-G = nx.MultiDiGraph()
+G = nx.Graph()
+start = "lol"
+end = "kek"
 for i in range(len(s)):
-    link = s[i].split(' ')
-    if len(link) >= 3:
-        G.add_edge(int(link[0]), int(link[1]), capacity=1, weight=int(link[2]))
+    if '#' in s[i]:
+        if '##start' in s[i]:
+            i += 1
+            words = s[i].split(' ')
+            start = words[0]
+            G.add_node(words[0], demand=-kolichestvo_muraviev)
+            continue
+        elif '##end' in s[i]:
+            i += 1
+            words = s[i].split(' ')
+            end = words[0]
+            G.add_node(words[0], demand=kolichestvo_muraviev)
+            continue
+        else:
+            continue
+    if '-' not in s[i]:
+        words = s[i].split(' ')
+        G.add_node(words[0])
+    else:
+        link = s[i].split('-')
+        G.add_edge(link[0], link[1], capacity=1, weight=1)
 
-path = nx.dijkstra_path(G, start, end)
-path_len = nx.dijkstra_path_length(G, start, end)
 
-out = open("path.ref.test", 'w')
+def count_links(graph):
+    res = 0
+    for node in graph:
+        res += len(node)
+    return res
 
-out.write("Len: " + str(path_len) + '\n')
 
-for nd in path:
-    out.write(str(nd) + '\n')
+print(start)
+print(end)
+
+# nx.draw(G)
+# matplotlib.pyplot.show()
+
+
+print()
+# print(nx.algorithms.flow.maximum_flow(G, ns, ne))
+print(nx.algorithms.flow.maximum_flow_value(G, start, end))
+max_flow = nx.algorithms.flow.maximum_flow(G, start, end)
+print(nx.algorithms.flow.cost_of_flow(G, max_flow[1]))
+
+# print(nx.algorithms.max_flow_min_cost(G, ns, ne))
+
+
+# import time
+# ts = time.time()
+# rs = bell_ford(grph)
+# te = time.time()
+# print_res(rs)
+
 
 
 
