9b198a60ac7ae9c5549b844d6bbd45eeb1f6590b
ggerardy <liftchampion@gmail.com>
2019-04-24 14:40:13 +0300

    Testing good. Fixed segfault
    Took 31 minutes

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,6 @@ TESTS=$(ls tests)
 for test in $TESTS
 do
 printf "\x1B[38;5;47m"$test"\x1B[0m\n"
-valgrind --leak-check=full --show-leak-kinds=all ./lem-in --vis < tests/$test
+valgrind --leak-check=full --show-leak-kinds=all ./lem-in --format=minimal --fast < tests/$test
 echo ""
 done
--- srcs/ft_parser.c
+++ srcs/ft_parser.c
@@ -12,6 +12,16 @@
 
 #include "len_in.h"
 
+static inline int ft_parser_mallocs(t_data *dt)
+{
+	if (!(dt->heap = make_heap(dt->nodes->len)) ||
+		!(dt->dsts = ft_memalloc(sizeof(int) * dt->nodes->len)) ||
+		!(dt->ants = ft_memalloc(sizeof(t_ant) * dt->ant_count)) ||
+		!(dt->gone_ants = ft_memalloc(dt->ant_count)))
+		return (0);
+	return (1);
+}
+
 t_data	*ft_parser(int fd, t_pars *prs)
 {
 	t_data		*dt;
@@ -26,16 +36,13 @@ t_data	*ft_parser(int fd, t_pars *prs)
 		return ((void*)(size_t)ft_free_data(dt, 0));
 	if (!ft_check_start_end(dt))
 		return ((void*)(size_t)ft_free_data(dt, 0));
+	if (!ft_parser_mallocs(dt))
+		return ((void*)(size_t)ft_free_data(dt, 0));
 	if (parse_res == -1)
 		return (dt + 0 * ft_get_next_line(fd, 0, dt->buff_size));
 	if (!ft_parse_links(dt, fd))
 		return ((void*)(size_t)ft_free_data(dt, 0));
-	if (!(dt->heap = make_heap(dt->nodes->len)) ||
-		!(dt->dsts = ft_memalloc(sizeof(int) * dt->nodes->len)) ||
-		!(dt->ants = ft_memalloc(sizeof(t_ant) * dt->ant_count)) ||
-		!(dt->gone_ants = ft_memalloc(dt->ant_count)) ||
-		!ft_string_push_back(&dt->output, '\n') ||
-		(GET_FMT_A(prs->flags) &&
+	if ((GET_FMT_A(prs->flags) &&
 		!(dt->output = ft_make_string(INIT_OUTPUT_SIZE))))
 		return ((void*)(size_t)ft_free_data(dt, 0));
 	return (dt + 0 * ft_get_next_line(fd, 0, dt->buff_size));
