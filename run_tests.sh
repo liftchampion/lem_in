2af2d4300d6ef2904e41e93cc08debfd81d076e9
ggerardy <liftchampion@gmail.com>
2019-05-19 22:42:09 +0300

    Deleted unnecessary. Added tests

--- run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TESTS=$(ls tests)
-for test in $TESTS
-do
-printf "\x1B[38;5;47m"$test"\x1B[0m\n"
-valgrind --leak-check=full --show-leak-kinds=all ./lem-in --format=minimal --vis < tests/$test
-echo ""
-done
